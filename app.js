05bcdf4499e9b0bdc9049f4487dc48bab9c64d48
hamsterofdark <mlandryviolin@gmail.com>
2016-05-12 14:26:17 -0400

    got mongo and mongoose configed

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
-var express = require('express');
+var express = require('express'),
+	mongoose = require('mongoose'),
+	bodyParser = require('body-parser');
 
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+
+var Book = require('./models/bookModel');
 var app = express();
 
 var port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+
+var bookRouter = express.Router();
+
+bookRouter.route('/Books')
+	.post(function(req, res)
+	{
+		var book = new Book(req.body);
+
+		console.log(book);
+		res.send(book);
+
+	})
+	.get(function(req, res)
+	{
+		Book.find(function(err,books)
+			{
+				if(err)
+					console.log(err)
+				else
+					res.json(books);
+			});
+	});
+
+app.use('/api', bookRouter);
 
 app.get('/', function(req, res)
 {
