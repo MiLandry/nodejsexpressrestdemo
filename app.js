e287f4f98a860af84ff3324c3ecb5dc1cc1bec86
hamsterofdark <mlandryviolin@gmail.com>
2016-05-13 21:19:55 -0400

    refactored code and added posting abililty

--- app.js
+++ app.js
@@ -13,53 +13,12 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 
-var bookRouter = express.Router();
-
-bookRouter.route('/Books')
-	.post(function(req, res)
-	{
-		var book = new Book(req.body);
-
-		console.log(book);
-		res.send(book);
-
-	})
-	.get(function(req, res)
-	{
-
-		var query = {};
-
-		if (req.query.genre)
-		{
-			query.genre = req.query.genre;
-
-		}
-
-		Book.find(query, function(err,books)
-			{
-				if(err)
-					console.log(err)
-				else
-					res.json(books);
-			});
-	});
-
-bookRouter.route('/Books/:bookId')
-	.get(function(req, res)
-	{
+bookRouter = require('./Routes/bookRoutes')(Book);
 
 
-		Book.findById(req.params.bookId, function(err,books)
-			{
-				if(err)
-					console.log(err)
-				else
-					res.json(books);
-			});
-	});
 
 app.use('/api', bookRouter);
-
+//app.use('/api/authors', authorRouter);
 
 
 app.get('/', function(req, res)
